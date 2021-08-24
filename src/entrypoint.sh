d3603ac90448516045b2d4698258e501c08f8e33
alex <av@orbis3.com>
2021-08-24 12:34:39 +0300

    fix entrypoint

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -6,5 +6,4 @@ pip3 install -r /opt/requirements.txt
 cd /opt
 python manage.py makemigrations
 python manage.py migrate
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
 cd /opt && python manage.py runserver 0.0.0.0:9000
